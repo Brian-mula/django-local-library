a2b125692b6aeb826757ed9e7c1d48273a80ec79
Brian-mula <mulatiwangilabrian@gmail.com>
2022-08-23 22:59:10 +0300

    created a skeleton website

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # register catalog page
+    'catalog.apps.CatalogConfig'
 ]
 
 MIDDLEWARE = [
